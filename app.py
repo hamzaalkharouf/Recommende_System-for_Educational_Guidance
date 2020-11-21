0ee2a9fac930fb8d2853885815714517fbde83fe
hamzaalkharouf <64504487+hamzaalkharouf@users.noreply.github.com>
2020-11-21 15:15:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ app = Flask(__name__, static_folder=None)
 
 # to run =>  py app.py
 if __name__ == '__main__':
-
     app.register_blueprint(home)
     app.register_blueprint(user)
     app.run(port=5000)
